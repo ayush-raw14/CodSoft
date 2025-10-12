6ae027335521b08722db94185a911bb2791aa3a8
ayush-raw14 <ayushrawat.aug@gmail.com>
2025-10-12 16:34:51 +0530

    complete the functionality using js

---
+++ Task3_Calculator/jscript.js
@@ -0,0 +1,21 @@
+let outputScreen = document.getElementById("outputscr");
+
+function display(num) {
+    outputScreen.value += num;
+}
+
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } catch (err) {
+        alert("Invalid Calculation");
+    }
+}
+
+function clearScr() {
+    outputScreen.value = "";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
